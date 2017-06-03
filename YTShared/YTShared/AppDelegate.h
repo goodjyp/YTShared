25923e0348b86d2fdc70e42aa8f6b064b878bd30
JiYaPing <jiyaping@palmyou.com>
2017-06-03 20:45:23 +0800

    cocoapods

---
+++ YTShared/YTShared/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  YTShared
+//
+//  Created by ypj on 2017/6/3.
+//  Copyright © 2017年 ypj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
